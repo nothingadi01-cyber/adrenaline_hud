3f20011e4e92c3e5a0d792c314bed8c8fc584462
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-23 11:24:26 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+Config.Author = "Adi"
+Config.HUDName = "ADRENALINE"
+
+Config.EnableADI = true        -- AI Assistant
+Config.EnableEKG = true        -- Heart Rate
+Config.EnableSonar = true      -- Sound Detection
+Config.EnableMissionUI = true  -- Holographic Missions
+
+Config.Colors = {
+    primary = "#00ecff",
+    secondary = "#ff0055"
+}
