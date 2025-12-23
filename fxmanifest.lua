9d7ed29b01d46a28471103ef9075d93defb28d81
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-23 11:23:44 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Adi'
+description 'ADRENALINE HUD by Adi'
+version '1.5.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
